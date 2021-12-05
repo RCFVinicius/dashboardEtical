7a2c0b05341988a93b2b8d3d7ae6e7d105fa1fa8
vinic <viniciusvvv2@gmail.com>
2021-12-05 15:26:05 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,15 @@
+const led = document.getElementById('g1-1')
+led.style.cssText = 'background-color:rgba(0,255,0,1);'
+
+
+function maquina(mq){
+    switch(mq){
+        case '1':
+
+            break;
+
+        default:
+        console.log("Houve algum erro")
+        break;
+    }
+}
