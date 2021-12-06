f598046e4337d7ffe99de586e524f9752cb042f1
vinic <viniciusvvv2@gmail.com>
2021-12-05 21:04:20 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const led = document.getElementById('g1-1')
-led.style.cssText = 'background-color:rgba(0,255,0,1);'
-
-
 function timer(maquina, tempo){
     let mqn = `#g${maquina}-4`;
     let timer = document.querySelector(mqn);
     timer.innerHTML = tempo
 }
 
-timer(1, '99:99')
+//timer(1, '99:99')
 
 function statusF(maquina, status){
     let led1 = `#g${maquina}-1`
@@ -38,4 +34,9 @@ function statusF(maquina, status){
 
 }
 
-statusF(1, 'a')
+//statusF(1, 'a')
+document.querySelector('#espaco').style.cssText = 'background-color:rgba(255,0,0,0);'
+console.log('r')
+function render(pagina){
+    console.log(pagina)
+}
